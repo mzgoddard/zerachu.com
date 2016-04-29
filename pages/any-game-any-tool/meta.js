012a616e704f4c0d7181066434715aa3c4dfe964
Michael "Z" Goddard <mzgoddard@gmail.com>
2016-04-29 11:30:11 -0400

    Draft Any Game Any Tool

---
+++ pages/any-game-any-tool/meta.js
@@ -0,0 +1,5 @@
+module.exports = {
+  title: 'Any Game can be made with Any Tool but Every Tool cannot make Every Game',
+  created: '2016-04-29T04:10:17.449Z',
+  summary: 'Asking if a tool or library can make a certain game is hard to answer. I use to think you can give a solid answer to never use certain tools.',
+};
