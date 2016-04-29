c8d6c0b0ba35f8ae4ef9d8bdea64e559a21d96b5
Michael "Z" Goddard <mzgoddard@gmail.com>
2016-04-29 11:29:53 -0400

    Add lorem-ipsum page

--- pages/lorem-ipsum/meta.js
+++ pages/lorem-ipsum/meta.js
@@ -3,4 +3,6 @@ export default {
   blog: true,
   'public': '2016-04-08T13:48:55.414Z',
   created: '2016-04-08T13:48:55.414Z',
+  title: 'lorem ipsum',
+  summary: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.'
 };
